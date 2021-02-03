0b8e5d1cbd00786f5ea14acb20a02d2f819b76fd
asaf.itach <asafitach89@gmail.com>
2021-02-03 00:27:30 -0800

    initial commit

---
+++ filesm.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+void(){
+ for a in c cpp_fs ds projects quizzes system_programming
+ do
+   mkdir ~/git/a
+   touch ~/git/a/dummy.md
+ done
+ }
+
+void(){
+for a in include lib obj test
+do
+mkdir ~/git/cpp_fs/a
+   touch ~/git/cpp_fs/a/dummy.md
+   done
+   }
+   
+   void(){
+for a in include lib obj test
+do
+mkdir ~/git/ds/a
+   touch ~/git/cpp_fs/a/dummy.md
+   done
+   }
