683cba1c7e498a4454ee95420d9610b97c905d03
asaf.itach <asafitach89@gmail.com>
2021-05-03 14:40:22 +0300

    update

--- ds/src/sorts.c
+++ ds/src/sorts.c
@@ -468,44 +468,6 @@ void Qsort(void *base, size_t nitems, size_t size, int (*compar)(const void *, c
 
 
 
-/*
-// v is an array of elements to sort.
-// size is the number of elements in array
-// left and right is start and end of array
-//(*comp)(void*, void*) is a pointer to a function
-// which accepts two void* as its parameter
-void _qsort(void* v, int size, int left, int right,
-					int (*comp)(void*, void*))
-{
-	void *vt, *v3;
-	int i, last, mid = (left + right) / 2;
-	if (left >= right)
-		return;
 
-	// casting void* to char* so that operations
-	// can be done.
-	void* vl = (char*)(base + (left * size));
-	void* vr = (char*)(base + (mid * size));
-	swap(vl, vr, size);
-	last = left;
-	for (i = left + 1; i <= right; i++) {
-
-		// vl and vt will have the starting address
-		// of the elements which will be passed to
-		// comp function.
-		vt = (char*)(base + (i * size));
-		if ((*comp)(vl, vt) > 0) {
-			++last;
-			v3 = (char*)(base + (last * size));
-			swap(vt, v3, size);
-		}
-	}
-	v3 = (char*)(base + (last * size));
-	swap(vl, v3, size);
-	_qsort(v, size, left, last - 1, comp);
-	_qsort(v, size, last + 1, right, comp);
-}
-
-void PartitionAlgho(void *array, )*/
 
 /***********************************E.O.F.*************************************/
